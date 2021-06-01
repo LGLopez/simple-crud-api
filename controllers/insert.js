28ba6802c8eeeaef61fc63669f3d3c4b93ae9379
Luis Lopez <darkdestruktion@gmail.com>
2021-05-31 22:42:43 -0500

    insert now returns id from joke

--- controllers/insert.js
+++ controllers/insert.js
@@ -6,15 +6,16 @@ const handleInsert = (db) => (req, res) => {
     }
 
     db('jokes')
+        .returning('*')
         .insert({
             name: name,
             joke: joke,
             by_user: by_user,
             created_at: new Date(),
         })
-        .then(joke => res.json(joke[0]))
+        .then(joke => res.send({idJoke: joke[0].id}))
         .catch(err => res.status(400).json('Unable to add new joke'))
-    res.send({data: `Joke from ${by_user} added succesfully`})
+    //res.send({idJoke: `Joke from ${by_user} added succesfully`})
 }
 
 module.exports = {
